852d88b03914de028fb440cf7be1c9848d631992
Nadiia Katkova <nadezhda.katkova@gmail.com>
2022-08-07 17:34:56 +0200

    Task 5 (import csv file) (#4)
    * import path updated
    
    * name of shop changes

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <AppBar position="relative">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
-          <Link className={classes.homeLink} to="/">Updated Store!</Link>
+          <Link className={classes.homeLink} to="/">AWS Course (NADKTK) Store!</Link>
         </Typography>
 
         {auth && (
